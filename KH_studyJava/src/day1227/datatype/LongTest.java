bbf545f102ddbfb7792e655889708205ae3f0aac
euihyunee <lobil7782@gmail.com>
2024-12-27 17:01:29 +0900

    [create] LongTest About the scope and use of the Long type

--- KH_studyJava/src/day1227/datatype/LongTest.java
+++ KH_studyJava/src/day1227/datatype/LongTest.java
@@ -1,14 +1,20 @@
 package day1227.datatype;
 
-public class abc {
+import java.text.DecimalFormat;
+
+public class LongTest {
     public static void main(String[] args) {
         /**
          * long -> 관리하는 클래스를 만들었어요
          * java.lang.Long num;
          */
+        DecimalFormat formater = new DecimalFormat("###,###");
+
         System.out.print("long의 최소값 : ");
-        System.out.println(Long.MIN_VALUE);
+        System.out.println(formater.format(Long.MIN_VALUE));
         System.out.print("long의 최대값 : ");
-        System.out.println(Long.MAX_VALUE);
+        System.out.println(formater.format(Long.MAX_VALUE));
+        System.out.println("long의 사이즈 : " + Long.SIZE + "bit");
+        System.out.println("long의 byte : " + Long.BYTES + "byte");
     }
 }
