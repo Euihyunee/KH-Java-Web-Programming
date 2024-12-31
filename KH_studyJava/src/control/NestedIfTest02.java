19cec193a263cba67e8d3c8d0aa7f10f9c8ebd24
euihyunee <lobil7782@gmail.com>
2024-12-31 17:09:46 +0900

    [create] score assignment problem with if operatop

--- KH_studyJava/src/control/NestedIfTest02.java
+++ KH_studyJava/src/control/NestedIfTest02.java
@@ -1,2 +1,27 @@
-package control;public class NestedIfTest02 {
+package control;
+
+import java.util.Scanner;
+
+public class NestedIfTest02 {
+    public static void main(String[] args) {
+        System.out.print("점수를 입력해주세요(0~100):");
+        System.out.print("입력 > ");
+        Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+        scanner.close();
+
+
+
+        if (number>=0 && number <=100) {
+            if (number >= 90) {
+                System.out.println("점수A");
+            } else if (number >= 80) {
+                System.out.println("점수B");
+            } else if (number >= 70) {
+                System.out.println("점수C");
+            } else if (number >= 60) {
+                System.out.println("점수D");
+            }else System.out.println("점수F");
+        }else System.out.println("0부터 100사이 입력해주세요!");
+    }
 }
