03d47bab9ddc51c0d4f28c19f5c838af509a3203
euihyunee <lobil7782@gmail.com>
2024-12-31 17:08:17 +0900

    [create] learning the conditional operator if

--- KH_studyJava/src/control/IfTest.java
+++ KH_studyJava/src/control/IfTest.java
@@ -1,2 +1,20 @@
-package control;public class IfTest {
+package control;
+
+import java.util.Scanner;
+
+// 제어문
+public class IfTest {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        scanner.close();
+
+        if (number < 0) {
+            System.out.println("0보다 작습니다.");
+        } else if (number == 0) {
+            System.out.println("0입니다.");
+        }else System.out.println("0보다 큽니다.");
+    }
 }
