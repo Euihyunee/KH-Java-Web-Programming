1ad46ac032dc3e9fc10ffabcd2b8d64ce763c40f
euihyunee <lobil7782@gmail.com>
2025-01-02 15:10:38 +0900

    [create] learning about for statements

---
+++ KH_studyJava/src/loops/ForTTest01.java
@@ -0,0 +1,9 @@
+package loops;
+
+public class ForTTest01 {
+    public static void main(String[] args) {
+        for(int i =1; i<10; i++){
+            System.out.println("루프 : " + i + "번");
+        }
+    }
+}
