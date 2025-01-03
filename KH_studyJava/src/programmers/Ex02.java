dcf4159b6bcee7b7046ce99c7bdf771601f35f85
euihyunee <lobil7782@gmail.com>
2025-01-03 17:25:40 +0900

    [create] explanation about 프로그래머스 배열만들기1

--- KH_studyJava/src/programmers/Ex02.java
+++ KH_studyJava/src/programmers/Ex02.java
@@ -1,12 +1,14 @@
 package programmers;
 
+import java.util.Arrays;
+
 public class Ex02 {
     public static void main(String[] args) {
 
         Solution solution = new Solution();
         int[] array = solution.solution(10, 3);
         for(int i : array) {
-            System.out.println(i);
+            System.out.print(i + " ");
         }
     }
 }
@@ -16,10 +18,17 @@ class Solution {
 
         for(int i = 1; i <= n; i++) {
             if (i % k == 0) {
-                int[] temp = new int[answer.length + 1];
-                for(int j=0; j<answer.length; j++) {
-                    temp[j] = answer[j];
-                }
+
+                // TODO 1번째 방법
+                int[] temp = Arrays.copyOf(answer, answer.length + 1);
+                // TODO 2번째 방법
+//                int[] temp = new int[answer.length + 1];
+//                System.arraycopy(answer, 0, temp, 0, answer.length);
+                // TODO 3번째 방법 answer -> temp 수동 복사
+//                 int[] temp = new int[answer.length + 1];
+//                 for(int j=0; j<answer.length; j++) {
+//                    temp[j] = answer[j];
+//                }
 
                 temp[temp.length - 1] = i;
                 answer = temp;
