e2644084290e95c18e917609c97ae1c8446fd989
euihyunee <lobil7782@gmail.com>
2025-01-10 12:33:05 +0900

    [learn] about BufferedLeader and Scanner

--- KH_studyJava/src/input/ReaderWriterTest.java
+++ KH_studyJava/src/input/ReaderWriterTest.java
@@ -1,2 +1,35 @@
-package input;public class ReaderWriterTest {
+package input;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+class Post{
+    String title;
+    String content;
+}
+
+public class ReaderWriterTest {
+    public static void main(String[] args) throws IOException {
+
+        // 알고리즘 관련 처리-성능 고려
+        // Scanner 대신 BufferdReader
+        // 문자 단위 Stream
+        // InputStreamReader isr;
+        // 버스와 관련있음
+
+        // InputStreamReader는 추상 Reader를 상속받음
+        // BufferedReader 생성자에는 Reader(System.in) 필요
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Post post = new Post();
+        System.out.print("타이틀 입력: ");
+        post.title = br.readLine();
+        System.out.print("내용 입력: ");
+        post.content = br.readLine();
+        br.close();
+        System.out.println("title: " + post.title);
+        System.out.println("content: " + post.content);
+    }
 }
--- KH_studyJava/src/input/ScannerTest03.java
+++ KH_studyJava/src/input/ScannerTest03.java
@@ -1,2 +1,27 @@
-package input;public class ScannerTest03 {
+package input;
+
+import java.util.Scanner;
+
+class Person{
+    String name;
+    String phone;
+}
+
+
+public class ScannerTest03 {
+    public static void main(String[] args) {
+        Person p = new Person();
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("이름 입력: ");
+        // 뒤에 오는 엔터까지 버퍼에서 데이터를 읽음
+        // nextLine()을 대신하는 방법은 BufferedLeader
+        p.name = sc.nextLine();
+        System.out.print("전화번호 입력: ");
+        p.phone = sc.nextLine();
+        sc.close();
+        System.out.println("--print--");
+        System.out.println("이름 : " + p.name);
+        System.out.println("전화번호 : " + p.phone);
+    }
 }
